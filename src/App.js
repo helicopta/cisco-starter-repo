3a60164204739eb354f73eba36c859f5115c3a5f
helicopta <helicopta@users.noreply.github.com>
2023-05-22 22:40:11 -0400

    front

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Banner from './Banner'; 
+import Exhibit from './Exhibit';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <Exhibit />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Exhibit.js
@@ -0,0 +1,3 @@
+export default function Exhibit () {
+    return <div>Exhibit</div>
+}
